eadee50e5feae450db94c032447d57c800ef53c1
Dea Salsabilla <deasalsabilla19@gmail.com>
2023-07-18 19:23:46 +0700

    mengganti kesalahan link active

--- admin/assets/conn/koneksi.php
+++ admin/assets/conn/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "desabangowan");
+$koneksi = mysqli_connect("sql201.infinityfree.com", "if0_34614694", "lMQ73OHDoVsHwp", "if0_34614694_desabangowan");
 
 // Check connection
 if (mysqli_connect_errno()) {
