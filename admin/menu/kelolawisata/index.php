43a6790ab0ea5f844f97ec2e5f27953f0fc7243e
Dea Salsabilla <deasalsabilla19@gmail.com>
2023-07-21 15:07:01 +0700

    mengganti title di kelola wisata

--- admin/menu/kelolawisata/index.php
+++ admin/menu/kelolawisata/index.php
@@ -10,7 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil - Bukit kunci</title>
+    <title>Kelola Wisata - Wisata Desa Bangowan</title>
     <link rel="stylesheet" href="../../assets/app/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../assets/icons/css/font-awesome.min.css">
     <link rel="stylesheet" href="../../dist/css/index.css">
