1b4060c76acb4724d133fe3d31bdf4672759c14f
Dea Salsabilla <deasalsabilla19@gmail.com>
2023-07-22 14:46:23 +0700

    menghapus semua comment di tambah wisata

--- admin/menu/kelolawisata/index.php
+++ admin/menu/kelolawisata/index.php
@@ -30,71 +30,6 @@ session_start();
                     <i class="fa fa-bars fa-lg" aria-hidden="true"></i>
                 </button>
                 <a class="navbar-brand me-auto text-danger" href="#">Desa<span class="text-warning">Bangowan</span></a>
-                <!-- <ul class="nav ms-auto">
-                    <li class="nav-item dropstart">
-                        <a class="nav-link text-dark ps-3" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
-                            <i class="fa fa-bell fa-lg py-2" aria-hidden="true"></i>
-                            <span class="badge bg-danger">10</span>
-                        </a>
-                        <div class="dropdown-menu mt-2 pt-0" aria-labelledby="navbarDropdown">
-                            <div class="d-flex p-3 border-bottom align-items-cente mb-2">
-                                <i class="fa fa-bell me-3" aria-hidden="true"></i>
-                                <span class="fw-bold lh-1">Notifikasi</span>
-                            </div>
-                            <a class="dropdown-item py-2" href="#">
-                                <div class="d-flex overflow-hidden">
-                                    <i class="fa fa-envelope fa-lg dropdown-icons bg-primary text-white p-2 me-2" aria-hidden="true"></i>
-                                    <div class="d-block content">
-                                        <p class="lh-1 mb-0">Safna Prasetiono</p>
-                                        <span class="content-text">p.</span>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item py-2" href="#">
-                                <div class="d-flex overflow-hidden">
-                                    <i class="fa fa-file fa-lg dropdown-icons bg-warning text-white p-2 me-2" aria-hidden="true"></i>
-                                    <div class="d-block content">
-                                        <p class="lh-1 mb-0">file informations</p>
-                                        <span class="content-text">your file has exceeded the limit.</span>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item py-2" href="#">
-                                <div class="d-flex overflow-hidden">
-                                    <i class="fa fa-hdd-o fa-lg dropdown-icons bg-danger text-white p-2 me-2" aria-hidden="true"></i>
-                                    <div class="d-block content">
-                                        <p class="lh-1 mb-0">Storage</p>
-                                        <span class="content-text">Your storage is full, delete some files.</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropstart">
-                        <a class="nav-link text-dark ps-3 pe-1" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
-                            <img src="../../images/user/user.png" alt="user" class="img-user">
-                        </a>
-                        <div class="dropdown-menu mt-2 pt-0" aria-labelledby="navbarDropdown">
-                            <div class="d-flex p-3 border-bottom mb-2">
-                                <img src="../../images/user/user.png" alt="user" class="img-user me-2">
-                                <div class="d-block">
-                                    <p class="fw-bold m-0 lh-1">YourName</p>
-                                    <small>YourAccount@gmail.com</small>
-                                </div>
-                            </div>
-                            <a class="dropdown-item" href="#">
-                                <i class="fa fa-user fa-lg me-3" aria-hidden="true"></i>Profile
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fa fa-cog fa-lg me-3" aria-hidden="true"></i>Setting
-                            </a>
-                            <hr class="dropdown-divider">
-                            <a class="dropdown-item" href="#">
-                                <i class="fa fa-sign-out fa-lg me-2" aria-hidden="true"></i>LogOut
-                            </a>
-                        </div>
-                    </li>
-                </ul> -->
             </div>
         </nav>
         <!-- end navbar-->
@@ -116,9 +51,6 @@ session_start();
                     </a>
                     <hr class="soft my-1 bg-dark">
 
-                    <!-- <a class="nav-link px-3" href="../../menu/profile/index.php">
-                        <i class="fa fa-user fa-lg box-icon" aria-hidden="true"></i>Profile
-                    </a> -->
                     <a class="nav-link px-3" href="../../menu/berita/index.php">
                         <i class="" aria-hidden="true"></i>Berita
                     </a>
@@ -130,9 +62,6 @@ session_start();
                         <a class="nav-link px-3" href="../../menu/kelolafoto/index.php">
                             <i class="" aria-hidden="true"></i>Kelola Foto
                         </a>
-                        <!-- <a class="nav-link px-3" href="../../menu/reservasi/index.php">
-                            <i class="" aria-hidden="true"></i>Reservasi
-                        </a> -->
 
                         <a class="nav-link px-3" href="../../menu/user/index.php">
                             <i class="" aria-hidden="true"></i>User
